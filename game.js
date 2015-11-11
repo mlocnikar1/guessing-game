38f52de957789ec90117e718009be24ebb3382f2
Administrator <administrator@Administrators-MacBook-Pro.local>
2015-11-11 15:29:58 -0800

    Added logic to insert the questions and results into the html of the page

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
-//create empty array variable to store user's answers
-var answers = [];
+window.onload = function(){ //wait until the page finishes loading to start the game
+	//create empty array variable to store user's answers
+	var answers = [];
 
-//create array with my 3 questions
+	//create array with my 3 questions
 	var questions = ["Will I be having tacos for dinner tonight?", 
 						"Is my favorite movie Bridesmaids?",
 						"Was I a ghost for Halloween?",
@@ -12,35 +13,52 @@ var answers = [];
 	//create array with the answers to my questions
 	var answerKey = ["Yes", "Yes", "No", "Yes", "No", 23];
 
-//Go through question array and answer and give user feedback about their answers
-for(var i = 0; i < questions.length; i++){
-	var questionNumber = i + 1;
-	//Get the user's answer
-	answers[i] = prompt(questions[i]);
-	if(i === questions.length - 1){ //the last question is not a yes or no question
+	var resultEl; //The element that holds the user's results 
+	var questionEl; //The element that holds the questions
+	
+	//Access div on page to hold the questions
+	questionEl = document.getElementById("questionEl");
+	//start with a blank question area each time the game is played
+	questionEl.innerHTML = "";
+
+	//Access div on page to hold the results
+	resultEl = document.getElementById("resultEl");
+	//start with a blank result area each time the game is played
+	resultEl.innerHTML = "";
+
+	//Go through question array and answer and give user feedback about their answers
+	for(var i = 0; i < questions.length; i++){
+		//Put the question on the page
+		questionEl.innerHTML += '<h3>Question ' + (i + 1)+ ': ' + questions[i] + '</h3>';
+		var questionNumber = i + 1;
+		//Get the user's answer
+		answers[i] = prompt(questions[i]);
+		//Process the user's answer
+		if(i === questions.length - 1){ //the last question is not a yes or no question
 			//convert the user's last answer to a number type
 			answers[i] = parseInt(answers[i]);
 			//console.log(answers[i]);
 			//verify the user entered one of the correct numbers
 			if(answers[i] !== 23 && answers[i] !== 65 && answers[i] !== 93){ 
-				console.log("I'm sorry, you didn't choose one of the available numbers. Please refresh the page to start over.");
+				resultEl.innerHTML += "<h3>I'm sorry, you didn't pick one of the given numbers in question 6. Please refresh the page to start over and be sure to answer 23, 65, or 93.</h3>";
 				break;
-	}else if(answers[i].toLowerCase() === "y" || answers[i].toLowerCase() === "yes"){
-		answers[i] = "Yes";
-	}else if(answers[i].toLowerCase() === "n" || answers[i].toLowerCase() === "no"){
-		answers[i] = "No";
-	}else{ //User did not answer yes or no - exit game
-		console.log("I'm sorry, I didn't understand your answer. Please refresh the page to start over and be sure to answer 'Yes' or 'No'");
-		break;
+			}
+		}else if(answers[i].toLowerCase() === "n" || answers[i].toLowerCase() === "no"){
+			answers[i] = "No";
+		}else if(answers[i].toLowerCase() === "y" || answers[i].toLowerCase() === "yes"){
+			answers[i] = "Yes";
+		}else{ //User did not answer yes or no - exit game
+			resultEl.innerHTML += "<h3>I'm sorry, I didn't understand your answer. Please refresh the page to start over and be sure to answer 'Yes' or 'No'</h3>";
+			break;
+		}
+		//Add the user's feedback, based on their answer, to the page under the results section
+		if(answers[i] === answerKey[i]){
+			resultEl.innerHTML += "<h3>Your answer to question " + questionNumber + " is correct! The answer to '" 
+				+ questions[i] + "' was '" + answers[i] + "'!</h3>";
+		}else{
+			resultEl.innerHTML += "<h3>Sorry, your answer to question " + questionNumber + " was wrong :(. The correct answer to '" + questions[i] + "' was '" + answerKey[i] + "'.</h3>";
+		}
 	}
-	//Give the user feedback based on their answer
-	if(answers[i] === answerKey[i]){
-		console.log("Your answer to question " + questionNumber + " is correct! The answer to '" 
-			+ questions[i] + "' was '" + answers[i] + "'!")
-	}else{
-		console.log("Sorry, your answer to question " + questionNumber + " was wrong :(. The correct answer to '" + questions[i] + "' was '" + answerKey[i] + "'.");
-	}
-}
-
+};
 
 
